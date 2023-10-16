a7763a6b8193d362cf244e82a1e6be6229e5c381
vivekgi <39313678+vivekgi@users.noreply.github.com>
2023-10-16 17:15:25 +0530

    Update codepractice.py

--- codepractice.py
+++ codepractice.py
@@ -21,3 +21,61 @@ st.title("Invoke API Endpoint and Print Status")
 # Create a button to invoke the API endpoint
 if st.button("Invoke API Endpoint"):
     invoke_endpoint()
+===================================  
+Add the following lines to your Django project's settings.py file:
+Python
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    },
+}
+Use code with caution. Learn more
+Create a new file called wsgi.py in the root directory of your Django project. Add the following code to this file:
+Python
+import os
+
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+application = get_default_application()
+Use code with caution. Learn more
+Create a new file called streamlit_app.py in the root directory of your Django project. Add your Streamlit code to this file.
+
+Run the following command to start the Django Channels server:
+
+python manage.py runserver
+Open a new terminal window and navigate to the directory containing your Django project.
+
+Run the following command to start the Streamlit application:
+
+streamlit run streamlit_app.py
+To run a Streamlit code in Django using a WSGI server, follow these steps:
+
+Install the Gunicorn WSGI server:
+pip install gunicorn
+Create a new file called gunicorn.conf.py in the root directory of your Django project. Add the following code to this file:
+Python
+bind = "0.0.0.0:8000"
+workers = 1
+
+wsgi_app = "mysite.wsgi:application"
+Use code with caution. Learn more
+Run the following command to start the Gunicorn server:
+gunicorn -c gunicorn.conf.py streamlit_app:app
+Open a new terminal window and navigate to the directory containing your Django project.
+
+Run the following command to start the Streamlit application:
+
+streamlit run streamlit_app.py
+Once you have started the Streamlit application, you can access it at http://localhost:8000 in your web browser.
+
+Tips
+If you are using Django Channels, you can use the channels module to communicate with your Streamlit applications. For example, you can use the channels.layers.send() function to send messages to your Streamlit applications.
+
+If you are using a WSGI server, you can use the flask.request and flask.response objects to communicate with your Streamlit applications. For example, you can use the flask.request.args object to get the arguments passed to the Streamlit application.
+
+You can also use Django's authentication and authorization system to protect your Streamlit applications. For example, you can use the @login_required decorator to require users to be logged in before they can access your Streamlit applications.
+
+Conclusion
+There are two main ways to run Streamlit code in Django: using Django Channels or using a WSGI server. The recommended method is to use Django Channels, as it is more scalable and allows you to communicate with your Streamlit applications using WebSockets.
